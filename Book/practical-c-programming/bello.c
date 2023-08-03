d162ae779149e5e7db87f4a7f024d44c2239599b
tonynguyenbk <truong.nd172877@sis.hust.edu.vn>
2023-08-03 23:57:03 +0700

    just edit a little bit

--- Book/practical-c-programming/bello.c
+++ Book/practical-c-programming/bello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main() {
-    printf("hello world!\n");
-    printf("Hi Tony");
+int term; /* term used in two expressions */
+int main()
+{
+term = 3 * 5;
+printf("Twice %d is %d\n", term, 2*term);
+printf("Three times %d is %d\n", term, 3*term);
+return (0);
 }
